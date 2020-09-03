0667865aee2899c6e0c0a99e0b35ee668c8dc41a
Christian Baumann <chriss713@gmail.com>
2020-09-03 17:47:27 +0200

    Added test for getting the day

--- sort_photos_test.rb
+++ sort_photos_test.rb
@@ -14,4 +14,8 @@ class SortPhotosTest < Test::Unit::TestCase
     assert_equal '08', @sort_photos.get_month('example_photos/IMG_6036.jpg'), "Months don't match"
   end
 
+  def test_get_day
+    assert_equal '27', @sort_photos.get_day('example_photos/IMG_6036.jpg'), "Days don't match"
+  end
+
 end
