b5c9ea2e04299da013a6ec824470f8faff4f7a5f
Christian Baumann <chriss713@gmail.com>
2020-09-03 18:09:52 +0200

    Refactored tests

--- sort_photos_test.rb
+++ sort_photos_test.rb
@@ -3,18 +3,20 @@ require_relative 'sort_photos'
 
 class SortPhotosTest < Test::Unit::TestCase
   def setup
+    @example_photo = 'example_photos/IMG_6036.jpg'
+
     @sort_photos = SortPhotos.new
   end
 
   def test_get_date
-    assert_equal '2020-08-27', @sort_photos.get_date('example_photos/IMG_6036.jpg'), "Dates don't match"
+    assert_equal '2020-08-27', @sort_photos.get_date(@example_photo), "Dates don't match"
   end
 
   def test_create_directory
-    date = @sort_photos.get_date('example_photos/IMG_6036.jpg')
+    date = @sort_photos.get_date(@example_photo)
     target_directory = @sort_photos.target_directory
 
-    assert_equal "#{target_directory}/#{date}", @sort_photos.create_directory('example_photos/IMG_6036.jpg')[0], "Directory path is not correct"
+    assert_equal "#{target_directory}/#{date}", @sort_photos.create_directory(@example_photo)[0], "Directory path is not correct"
     assert_equal true, File.directory?("#{target_directory}/#{date}"), "Directory doesn't exist"
   end
 
