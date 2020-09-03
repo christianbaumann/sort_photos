f94938d408b0e6c086310e5a1090686a7b9909b5
Christian Baumann <chriss713@gmail.com>
2020-09-03 18:07:45 +0200

    Creating target directory

--- sort_photos.rb
+++ sort_photos.rb
@@ -1,10 +1,22 @@
 require 'exifr/jpeg'
+require 'fileutils'
 
 class SortPhotos
+
+  attr_accessor :target_directory
+
+  def initialize
+    @target_directory = 'sorted'
+  end
 
   def get_date photo
     date_time = EXIFR::JPEG.new(photo).date_time
     date_time.to_s[0, 10]
+  end
+
+  def create_directory photo
+    date = get_date(photo)
+    FileUtils.mkdir_p "#{@target_directory}/#{date}"
   end
 
 end
--- sort_photos_test.rb
+++ sort_photos_test.rb
@@ -10,4 +10,12 @@ class SortPhotosTest < Test::Unit::TestCase
     assert_equal '2020-08-27', @sort_photos.get_date('example_photos/IMG_6036.jpg'), "Dates don't match"
   end
 
+  def test_create_directory
+    date = @sort_photos.get_date('example_photos/IMG_6036.jpg')
+    target_directory = @sort_photos.target_directory
+
+    assert_equal "#{target_directory}/#{date}", @sort_photos.create_directory('example_photos/IMG_6036.jpg')[0], "Directory path is not correct"
+    assert_equal true, File.directory?("#{target_directory}/#{date}"), "Directory doesn't exist"
+  end
+
 end
