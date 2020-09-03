1bc91fa33d8d9251a4eeda75cd1e4a9409b4842d
Christian Baumann <chriss713@gmail.com>
2020-09-03 17:44:12 +0200

    Added test for getting the month

--- sort_photos_test.rb
+++ sort_photos_test.rb
@@ -10,4 +10,8 @@ class SortPhotosTest < Test::Unit::TestCase
     assert_equal '2020', @sort_photos.get_year('example_photos/IMG_6036.jpg'), "Years don't match"
   end
 
+  def test_get_month
+    assert_equal '08', @sort_photos.get_month('example_photos/IMG_6036.jpg'), "Months don't match"
+  end
+
 end
