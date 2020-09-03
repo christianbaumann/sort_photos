9c7f075e8e251e845efd5634112e29d0969f8323
Christian Baumann <chriss713@gmail.com>
2020-09-03 17:54:04 +0200

    Refactored to only use one method to get the date

--- sort_photos.rb
+++ sort_photos.rb
@@ -2,14 +2,9 @@ require 'exifr/jpeg'
 
 class SortPhotos
 
-  def get_year photo
+  def get_date photo
     date_time = EXIFR::JPEG.new(photo).date_time
-    date_time.to_s[0, 4]
-  end
-
-  def get_month photo
-    date_time = EXIFR::JPEG.new(photo).date_time
-    date_time.to_s[5, 2]
+    date_time.to_s[0, 10]
   end
 
 end
--- sort_photos_test.rb
+++ sort_photos_test.rb
@@ -6,16 +6,8 @@ class SortPhotosTest < Test::Unit::TestCase
     @sort_photos = SortPhotos.new
   end
 
-  def test_get_year
-    assert_equal '2020', @sort_photos.get_year('example_photos/IMG_6036.jpg'), "Years don't match"
-  end
-
-  def test_get_month
-    assert_equal '08', @sort_photos.get_month('example_photos/IMG_6036.jpg'), "Months don't match"
-  end
-
-  def test_get_day
-    assert_equal '27', @sort_photos.get_day('example_photos/IMG_6036.jpg'), "Days don't match"
+  def test_get_date
+    assert_equal '2020-08-27', @sort_photos.get_date('example_photos/IMG_6036.jpg'), "Dates don't match"
   end
 
 end
