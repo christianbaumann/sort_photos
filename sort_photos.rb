74797f2433b5f5e122b2be471f08f97ba8beb03d
Christian Baumann <chriss713@gmail.com>
2020-09-03 17:46:31 +0200

    Getting the month

--- sort_photos.rb
+++ sort_photos.rb
@@ -4,7 +4,12 @@ class SortPhotos
 
   def get_year photo
     date_time = EXIFR::JPEG.new(photo).date_time
-    date_time.to_s[0,4]
+    date_time.to_s[0, 4]
+  end
+
+  def get_month photo
+    date_time = EXIFR::JPEG.new(photo).date_time
+    date_time.to_s[5, 2]
   end
 
 end
